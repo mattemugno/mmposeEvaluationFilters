ccc054d98e23318b744b68d9e681c03daf3c3c49
mattemugno <m.mugnai5@studenti.unipi.it>
2025-01-28 15:59:18 +0100

    add blur heatmaps

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -93,54 +93,59 @@ def main():
 
     bboxxes_path = "../data/coco/person_detection_results/COCO_val2017_detections_AP_H_56_person.json"
 
+    kernel_sizes = [5, 17, 29, 41, 53, 65, 77, 89, 101]
+
     # build the model from a config file and a checkpoint file
     if args.draw_heatmap:
         cfg_options = dict(model=dict(test_cfg=dict(output_heatmaps=True)))
     else:
         cfg_options = None
 
-    model = init_model(
-        args.config,
-        args.checkpoint,
-        device=args.device,
-        cfg_options=cfg_options)
+    for intensity in kernel_sizes:
 
-    # init visualizer
-    model.cfg.visualizer.radius = args.radius
-    model.cfg.visualizer.alpha = args.alpha
-    model.cfg.visualizer.line_width = args.thickness
+        model = init_model(
+            args.config + f'_{intensity}x{intensity}.py',
+            args.checkpoint,
+            device=args.device,
+            cfg_options=cfg_options)
 
-    visualizer = VISUALIZERS.build(model.cfg.visualizer)
-    visualizer.set_dataset_meta(
-        model.dataset_meta, skeleton_style=args.skeleton_style)
+        # init visualizer
+        model.cfg.visualizer.radius = args.radius
+        model.cfg.visualizer.alpha = args.alpha
+        model.cfg.visualizer.line_width = args.thickness
 
-    id = int(args.img.split('/')[-1].split('.')[0])
-    bboxes = _load_detection_results(bboxxes_path, id)
+        visualizer = VISUALIZERS.build(model.cfg.visualizer)
+        visualizer.set_dataset_meta(
+            model.dataset_meta, skeleton_style=args.skeleton_style)
 
-    # inference a single image
-    batch_results = inference_topdown(model, args.img, bboxes=bboxes)
-    results = merge_data_samples(batch_results)
+        id = int(args.img.split('/')[-1].split('.')[0])
+        bboxes = _load_detection_results(bboxxes_path, id)
 
-    # show the results
-    img = imread(args.img, channel_order='rgb')
-    visualizer.add_datasample(
-        'result',
-        cv2.GaussianBlur(img, (5, 5), 0),
-        data_sample=results,
-        draw_gt=False,
-        draw_bbox=True,
-        kpt_thr=args.kpt_thr,
-        draw_heatmap=args.draw_heatmap,
-        show_kpt_idx=args.show_kpt_idx,
-        skeleton_style=args.skeleton_style,
-        show=args.show,
-        out_file=args.out_file)
 
-    if args.out_file is not None:
-        print_log(
-            f'the output image has been saved at {args.out_file}',
-            logger='current',
-            level=logging.INFO)
+        # inference a single image
+        batch_results = inference_topdown(model, args.img, bboxes=bboxes)
+        results = merge_data_samples(batch_results)
+
+        # show the results
+        img = imread(args.img, channel_order='rgb')
+        visualizer.add_datasample(
+            'result',
+            cv2.GaussianBlur(img, (intensity, intensity), 0),
+            data_sample=results,
+            draw_gt=False,
+            draw_bbox=True,
+            kpt_thr=args.kpt_thr,
+            draw_heatmap=args.draw_heatmap,
+            show_kpt_idx=args.show_kpt_idx,
+            skeleton_style=args.skeleton_style,
+            show=args.show,
+            out_file=args.out_file + f'_{intensity}x{intensity}.jpg')
+
+        if args.out_file is not None:
+            print_log(
+                f'the output image has been saved at {args.out_file}',
+                logger='current',
+                level=logging.INFO)
 
 
 if __name__ == '__main__':
