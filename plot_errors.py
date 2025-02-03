710686d4260025ba3c68f09f496c3a4c57a46d8b
mattemugno <m.mugnai5@studenti.unipi.it>
2025-02-03 17:51:08 +0100

    update plot errors

--- plot_errors.py
+++ plot_errors.py
@@ -11,65 +11,11 @@ from scipy.interpolate import griddata
 matplotlib.use('qtagg')
 from mpl_toolkits.mplot3d import Axes3D
 
-data_root = "tools/json_results/rtmpose-l/blur/results"
 
 def load_data(json_file):
     with open(json_file, 'r') as f:
         data = json.load(f)
     return data
-
-
-def plot_3d(data_files):
-    plt.style.use('seaborn-v0_8')
-
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5), subplot_kw={'projection': '3d'})
-
-    all_x = []
-    all_y = []
-    all_z = []
-
-
-    for file in data_files:
-        data = load_data(file)
-        keypoints = list(data.keys())[10:]
-        errors = np.array([data[k] for k in keypoints])
-        intensity = float(file.split("_")[-1].split(".")[0].replace("blur", "").split("x")[0])
-
-        x_indices = np.arange(len(keypoints))
-        z = np.full_like(errors, intensity, dtype=float)
-
-        all_x.extend(x_indices)
-        all_y.extend(z)
-        all_z.extend(errors)
-
-    xi = np.linspace(min(all_x), max(all_x), 50)
-    yi = np.linspace(min(all_y), max(all_y), 50)
-    X, Y = np.meshgrid(xi, yi)
-    Z = griddata((all_x, all_y), all_z, (X, Y), method='cubic')
-
-    for ax in axes:
-        ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
-        ax.set_xticks(np.arange(len(keypoints)))
-        ax.set_xticklabels(np.arange(0, 17), ha='right', fontsize=10)
-        ax.tick_params(axis='x', labelsize=9)
-        ax.set_ylabel('Kernel Size', fontsize=12)
-        ax.set_xlabel('Keypoint', fontsize=12)
-        ax.set_zlabel('Error', fontsize=12)
-
-        ax.set_facecolor('#f4f4f4')
-        ax.grid(True, linestyle=':', color='gray', linewidth=0.5)
-
-    # Titles and viewing angles
-    axes[0].set_title("View 1", fontsize=14)
-    axes[1].set_title("View 2", fontsize=14)
-    axes[2].set_title("View 3", fontsize=14)
-
-    axes[0].view_init(elev=20, azim=30)
-    axes[1].view_init(elev=30, azim=60)
-    axes[2].view_init(elev=40, azim=90)
-
-    plt.show()
-
 
 def plot_heatmap(data_files):
     data = []
@@ -139,6 +85,96 @@ def plot_3d_surface(data_files):
 
     plt.show()
 
+def plot_3d(data_files):
+
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5), subplot_kw={'projection': '3d'})
+
+    fig.patch.set_facecolor('white')
+
+    all_x = []
+    all_y = []
+    all_z = []
+
+    keypoint_names = ["Nose", "LEye", "REye", "LEar", "REar",
+                      "LShoulder", "RShoulder", "LElbow", "RElbow",
+                      "LWrist", "RWrist", "LHip", "RHip",
+                      "LKnee", "RKnee", "LAnkle", "RAnkle"]
+
+    for file in data_files:
+        data = load_data(file)
+        keypoints = list(data.keys())[10:]
+        errors = np.array([data[k] for k in keypoints])
+
+        intensity = int(file.split("_")[-1].split(".")[0].split("x")[0])
+
+        x_indices = np.arange(len(keypoints))
+        z = np.full_like(errors, intensity, dtype=float)
+
+        all_x.extend(x_indices)
+        all_y.extend(z)
+        all_z.extend(errors)
+
+    xi = np.linspace(min(all_x), max(all_x), 50)
+    yi = np.linspace(min(all_y), max(all_y), 50)
+    X, Y = np.meshgrid(xi, yi)
+    Z = griddata((all_x, all_y), all_z, (X, Y), method='cubic')
+
+    for i, ax in enumerate(axes):
+        ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
+
+        ax.set_facecolor('white')
+
+        # Rimuove il background grigio dalle 'panne' degli assi 3D
+        ax.xaxis.pane.fill = False
+        ax.yaxis.pane.fill = False
+        ax.zaxis.pane.fill = False
+
+        # Imposta il colore di sfondo dei bordi degli assi
+        ax.xaxis.pane.set_edgecolor('white')
+        ax.yaxis.pane.set_edgecolor('white')
+        ax.zaxis.pane.set_edgecolor('white')
+
+        # ax.set_xticks(np.arange(len(keypoints)))
+        # ax.set_xticklabels(np.arange(0, 17), ha='right', fontsize=10)
+        ax.set_xticks(np.arange(len(keypoint_names)))
+
+        if i == 0:
+            ax.set_xticklabels(keypoint_names, ha='left', fontsize=9)
+        else:
+            ax.set_xticklabels(keypoint_names, ha='right', fontsize=9, rotation=90)
+
+        # ax.set_xticklabels(keypoint_names, ha='right', fontsize=9, rotation=90)
+
+        ax.tick_params(axis='x', labelsize=9)
+        # ax.set_ylabel('Kernel Size', fontsize=12)
+        ax.set_ylabel('Pixel Size', fontsize=12)
+        # ax.set_xlabel('Keypoint', fontsize=12)
+        ax.set_zlabel('Error', fontsize=12)
+
+        ax.set_facecolor('#f4f4f4')
+        ax.grid(True, linestyle=':', color='white', linewidth=0.5)
+
+    fig.suptitle("RTMPose-L Blur", fontsize=16)
+
+    axes[0].view_init(elev=20, azim=20)
+    axes[1].view_init(elev=30, azim=60)
+    axes[2].view_init(elev=40, azim=90)
+
+    # Aggiunta della legenda con gli indici dei keypoints
+    #legend_text = "\n".join([f"{i}: {name}" for i, name in enumerate(keypoint_names)])
+    #fig.text(0.82, 0.5, legend_text, fontsize=10, verticalalignment='center',
+    #         bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.3'))
+
+    fig.savefig("RTMPose-L Blur.png", dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+
+
+models = ["hrnet-384x288", "rtmpose-l", "vitpose-b"]
+filter = ["blur", "pixelate"]
+
+data_root = "tools/json_results/rtmpose-l/blur/results"
 
 data_files = [os.path.join(data_root, file) for file in os.listdir(data_root)]
 
